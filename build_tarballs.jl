68b8363e3e9ce432cf6d35d50c2b6c1677b71a8c
Abel Soares Siqueira <abel.s.siqueira@gmail.com>
2019-01-10 07:05:24 -0500

    Expand gcc and remove a lot of platforms

--- build_tarballs.jl
+++ build_tarballs.jl
@@ -107,21 +107,21 @@ done
 # These are the platforms we will build for by default, unless further
 # platforms are passed in on the command line.
 #platforms = supported_platforms()
-#platforms = expand_gcc_versions(platforms)
 platforms = [
-    Linux(:i686, libc=:glibc),
+    #Linux(:i686, libc=:glibc),
     Linux(:x86_64, libc=:glibc),
-    Linux(:aarch64, libc=:glibc),
-    Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
-    Linux(:powerpc64le, libc=:glibc),
-    Linux(:i686, libc=:musl),
-    Linux(:x86_64, libc=:musl),
-    Linux(:aarch64, libc=:musl),
-    Linux(:armv7l, libc=:musl, call_abi=:eabihf),
+    #Linux(:aarch64, libc=:glibc),
+    #Linux(:armv7l, libc=:glibc, call_abi=:eabihf),
+    #Linux(:powerpc64le, libc=:glibc),
+    #Linux(:i686, libc=:musl),
+    #Linux(:x86_64, libc=:musl),
+    #Linux(:aarch64, libc=:musl),
+    #Linux(:armv7l, libc=:musl, call_abi=:eabihf),
     MacOS(:x86_64),
-    Windows(:i686),
-    Windows(:x86_64)
+    #Windows(:i686),
+    #Windows(:x86_64)
 ]
+platforms = expand_gcc_versions(platforms)
 
 
 # The products that we will ensure are always built
